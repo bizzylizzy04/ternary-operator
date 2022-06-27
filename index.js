d83066ad75d5f9090f1c748ac79086b49d5e7ab3
Liz <bizzylizzy04@users.noreply.github.com>
2022-06-27 10:11:53 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+let favoritePhrase = 'Love That!';
+result = (!favoritePhrase) ? console.log("I love that!") : console.log("I don't love that!");
+
+let age = 7;
+let kindergartenEligible = (age < 5) ? console.log("Too young") : console.log("Old Enough");
